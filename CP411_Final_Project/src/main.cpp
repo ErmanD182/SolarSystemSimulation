2f2939beee26a88850ce87f5dcda1a0926e58ae8
StuartIsley <isle7220@mylaurier.ca>
2018-11-26 13:03:54 -0500

    dwight you ignorant slut

--- CP411_Final_Project/src/main.cpp
+++ CP411_Final_Project/src/main.cpp
@@ -2,9 +2,25 @@
  * main.cpp
  *
  *  Created on: Nov 26, 2018
- *      Author: erman
+ *      Author: erman is a stupid idoit...
  */
+#include <GL/gl.h>
+#include <GL/glut.h>
 
+void init(void) {
+    glClearColor(1.0, 1.0, 1.0, 0.0); // Set display-window color to white
+    glMatrixMode(GL_PROJECTION);
+    gluOrtho2D(0.0, 640, 480, 0.0);
+    glutCreateWindow("SolarSyetem");
+    glColor3f(1.0, 0.0, 0.0);
+    glFlush();
+}
+int main(int argc, char** argv) {
+	glutInit(&argc,argv);
+
+	init();
+	glutMainLoop();
+}
 
 
 
