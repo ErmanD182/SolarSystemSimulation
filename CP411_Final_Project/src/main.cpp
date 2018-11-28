fb8a648218f0bb1c81510eef4e201375451777c4
StuartIsley <isle7220@mylaurier.ca>
2018-11-28 13:01:37 -0500

    i dont even know what this is commiting

--- CP411_Final_Project/src/main.cpp
+++ CP411_Final_Project/src/main.cpp
@@ -18,30 +18,17 @@
 #include "solarsystem.hpp"
 #include "circle.hpp"
 
-cirlce c;
+circle c;
 GLint winWidth = 1200, winHeight = 750;
 
 
 
 void init(void) {
-	glClearColor(1.0, 1.0, 1.0, 0.0);
-	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
-	glutInitWindowPosition(150, 20);
-	glutInitWindowSize(winWidth, winHeight);
-	glutCreateWindow("Solar System Simulator");
-	glutSwapBuffers();
-
-}
-
-
-void winReshapeFcn(GLint newWidth, GLint newHeight) {
-	glViewport(0, 0, newWidth, newHeight);
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	gluOrtho2D(0.0, GLdouble(newWidth), GLdouble(newHeight), 0.0);
-	winWidth = newWidth;
-	winHeight = newHeight;
-	glFlush();
+    glClearColor(0.0, 0.0, 0.0, 1.0); // Set display-window color to white
+    glMatrixMode(GL_PROJECTION);
+    gluOrtho2D(0.0, winWidth, winHeight, 0.0);
+    glColor3f(1.0, 0.0, 0.0);
+    glutSwapBuffers();
 }
 
 void mouseAction(GLint button, GLint action, GLint x, GLint y) {
@@ -66,12 +53,32 @@ void draw(){
 	glFlush();
 }
 
+void winReshapeFcn(GLint newWidth, GLint newHeight) {
+    /* Reset viewport and projection parameters */
+    glViewport(0, 0, newWidth, newHeight);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluOrtho2D(0.0, GLdouble(newWidth), GLdouble(newHeight), 0.0);
+    /* Reset display-window size parameters. */
+    winWidth = newWidth;
+    winHeight = newHeight;
+    draw();
+    glFlush();
+}
+
+
+
 int main(int argc, char** argv) {
 
 	setvbuf(stdout, NULL, _IONBF, 0); //for printing on Eclipse console
 	setvbuf(stderr, NULL, _IONBF, 0);
 
 	glutInit(&argc, argv);
+
+	 glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+	 glutInitWindowPosition(100, 100);
+	 glutInitWindowSize(winWidth, winHeight);
+	 glutCreateWindow("SolarSystem");
 
 
 	init();
